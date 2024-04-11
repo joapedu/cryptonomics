a7bec8b9084c9e6bd3aa2c2df53c78d317e96e96
João Eduardo <joaoeduardobraga2@gmail.com>
2024-04-11 17:32:50 -0300

    feat: add path to reset password api route

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('register/', views.RegisterView.as_view(), name='register'),
+    path('reset_password', views.ResetPasswordView.as_view(), name='reset_password'),
 ]
