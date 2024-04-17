96d235ca6fc9d5587de3c217134674d483bedb75
João Eduardo <joaoeduardobraga2@gmail.com>
2024-04-17 17:10:40 -0300

    feat: apply of routes inside of app root

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,31 @@
 import './App.css';
-import { Container } from '@mui/material';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  AboutComponent,
+  ChatComponent,
+  CoinectionsComponent,
+  HomeComponent,
+  LoginComponent,
+  ProfileComponent,
+  SettingsComponent,
+  YourCoinsComponent
+} from './components';
 
 function App() {
-  return (
-    <div className="App">
-      <Container maxWidth="sm">teste</Container>
-        
-    </div>
+  return (<div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeComponent />} />
+        <Route path="profile" element={<ProfileComponent />} />
+        <Route path="settings" element={<SettingsComponent />} />
+        <Route path="yourcoins" element={<YourCoinsComponent />} />
+        <Route path="login" element={<LoginComponent />} />
+        <Route path="coinections" element={<CoinectionsComponent />} />
+        <Route path="chat" element={<ChatComponent />} />
+        <Route path="about" element={<AboutComponent />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
   );
 }
 
