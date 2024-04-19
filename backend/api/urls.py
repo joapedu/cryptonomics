233cecdba85be314250dffe91f7ff77411c2f44a
João Eduardo <joaoeduardobraga2@gmail.com>
2024-04-19 16:03:33 -0300

    feat: feito com objeto unico da api no url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,19 @@
 """Arquivo de urls da api"""
 from django.urls import path
+from django.urls import include
 
 from . import views
+
+fav_coins = ([
+    path('all/', views.FavCoinsView.as_view(), name='todas'),
+    path('detail/<int:pk>', views.FavCoinsDetailView.as_view(), name='detahe'),
+])
 
 urlpatterns = [
     path("", views.index, name="index"),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('register/', views.RegisterView.as_view(), name='register'),
-    path('reset_password', views.ResetPasswordView.as_view(), name='reset_password'),
+    path('reset_password/', views.ResetPasswordView.as_view(), name='reset_password'),
+    path('fav_coins/', include(fav_coins)),
 ]
