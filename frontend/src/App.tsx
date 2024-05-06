38e83cebf2df01dfb27e2992628f21062957c37c
João Eduardo <joaoeduardobraga2@gmail.com>
2024-05-06 18:43:03 -0300

    refactor: troca da raiz para adequar novo código

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,30 +2,24 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
   AboutComponent,
-  ChatComponent,
-  CoinectionsComponent,
-  HomeComponent,
   LoginComponent,
+  MainComponent,
   ProfileComponent,
   SettingsComponent,
-  YourCoinsComponent
 } from './components';
 
 function App() {
-  return (<div className="App">
+  return (<>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeComponent />} />
+        <Route path="/" element={<MainComponent />} />
         <Route path="profile" element={<ProfileComponent />} />
         <Route path="settings" element={<SettingsComponent />} />
-        <Route path="yourcoins" element={<YourCoinsComponent />} />
         <Route path="login" element={<LoginComponent />} />
-        <Route path="coinections" element={<CoinectionsComponent />} />
-        <Route path="chat" element={<ChatComponent />} />
         <Route path="about" element={<AboutComponent />} />
       </Routes>
     </BrowserRouter>
-  </div>
+    </>
   );
 }
 
